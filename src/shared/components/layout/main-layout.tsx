b279451987317bb6283f481bdb8867f763656460
rohitjangid01 <rohit.devteam@webdaire.com>
2025-05-26 16:38:56 +0530

    main page scroll issue fix

--- src/shared/components/layout/main-layout.tsx
+++ src/shared/components/layout/main-layout.tsx
@@ -24,17 +24,16 @@ function InnerLayout({ children }: { children: React.ReactNode }) {
       }
     };
 
-    handleResize(); // Run once on mount
+    handleResize();
     window.addEventListener('resize', handleResize);
 
     return () => window.removeEventListener('resize', handleResize);
   }, [dispatch]);
 
-  if (!hasMounted) return null; // Avoid mismatch during SSR
+  if (!hasMounted) return null;
 
   return (
     <div className="relative">
-      {/* Background Overlay behind Sidebar on Mobile */}
       {isMainSidebarOpen && (
         <div
           className="fixed inset-0 bg-gray-300 bg-opacity-50 z-40 lg:hidden"
@@ -48,7 +47,7 @@ function InnerLayout({ children }: { children: React.ReactNode }) {
         {/* Main Sidebar */}
         <SidebarMain />
         {/* Content Area */}
-        <main className="flex-1 p-6 ml-16 sm:ml-0 bg-gray-50 overflow-auto">{children}</main>
+        <main className="flex-1 p-6 ml-16 sm:ml-0 bg-gray-50 overflow-auto h-screen">{children}</main>
       </div>
     </div>
   );
